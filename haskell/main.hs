5fd52aff2b118e5a06209356b190abcda5f5d6b1
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2021-09-23 01:35:39 +0900

    Haskell: fix warnigs

--- haskell/main.hs
+++ haskell/main.hs
@@ -4,7 +4,7 @@ isMunchausen number n total cache
   | n > 0 = isMunchausen number (n `div` 10) (total + (cache !! (n `mod` 10))) cache
   | otherwise = number == total
 
+main :: IO ()
 main = do
   let cache = 0 : [i ^ i | i <- [1 .. 9]]
   mapM_ print $ filter (\i -> isMunchausen i i 0 cache) [0 .. 440000000]
-  return 0
