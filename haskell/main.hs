71d2f6c23d74ed8afd260ef10f53cbe43b19ff52
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2021-09-23 01:42:39 +0900

    Haskell: speed up
    ❯ cabal build && hyperfine --warmup 1  --runs 5 (cabal list-bin exe:speedtests)
    Benchmark #1: /.../dist-newstyle/build/aarch64-osx/ghc-8.10.7/SpeedTests-0.1.0.0/x/speedtests/opt/build/speedtests/speedtests
      Time (mean ± σ):      1.896 s ±  0.008 s    [User: 1.888 s, System: 0.005 s]
      Range (min … max):    1.883 s …  1.903 s    5 runs

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,10 +1,52 @@
-isMunchausen :: Int -> Int -> Int -> [Int] -> Bool
-isMunchausen number n total cache
-  | total > number = False
-  | n > 0 = isMunchausen number (n `div` 10) (total + (cache !! (n `mod` 10))) cache
-  | otherwise = number == total
+{-# LANGUAGE BangPatterns #-}
+
+module Main (main) where
+
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as I (fromList, (!))
+import Data.Vector.Unboxed (Vector)
+import qualified Data.Vector.Unboxed as V (filter, generate, mapM_, unsafeIndex)
 
 main :: IO ()
 main = do
-  let cache = 0 : [i ^ i | i <- [1 .. 9]]
-  mapM_ print $ filter (\i -> isMunchausen i i 0 cache) [0 .. 440000000]
+  let
+    cached =
+      -- id
+      -- listCached
+      -- mapCached
+      vectorCached
+    !f = cached powerOfItself
+  -- mapM_ print $ filter (isMunchausen f) [0 .. 440000000]
+  V.mapM_ print $ V.filter (isMunchausen f) $ V.generate 440000000 id
+
+isMunchausen :: (Int -> Int) -> Int -> Bool
+isMunchausen powerOfItselfCached n = go 0 n
+  where
+    go :: Int -> Int -> Bool
+    go acc m
+      | acc > n = False
+      | m > 0 =
+          let (rest, onesPlace) = quotRem m 10 in go (acc + powerOfItselfCached onesPlace) rest
+      | otherwise = n == acc
+
+powerOfItself :: Int -> Int
+powerOfItself 0 = 0
+powerOfItself i = i ^ i
+
+listCached :: (Int -> Int) -> Int -> Int
+listCached f = (cache !!)
+  where
+    cache :: [Int]
+    cache = map f [0 .. 9]
+
+mapCached :: (Int -> Int) -> Int -> Int
+mapCached f = (cache I.!)
+  where
+    cache :: IntMap Int
+    !cache = I.fromList $ map (\i -> (i, f i)) [0 .. 9]
+
+vectorCached :: (Int -> Int) -> Int -> Int
+vectorCached f = V.unsafeIndex cache
+  where
+    cache :: Vector Int
+    !cache = V.generate 10 f
